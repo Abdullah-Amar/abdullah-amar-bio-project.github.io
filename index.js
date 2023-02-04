a1abebb18530dd0c1ff9db07c23c70c64a4ecd7e
Abdullah Mahmood <abood.flyhi@gmail.com>
2023-02-04 21:12:33 +0400

    Added ability to detect for checkbox

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
 function onSubmit(e) {
     e.preventDefault()
-    console.log(document.querySelector("#pgenome").value)
     console.log("Hi :)")
+    
+    let Pgenome = document.querySelector("#pgenome").value
+    let Mgenome = document.querySelector("#mgenome").value
+    Pgenome = Pgenome.split("")
+    Mgenome = Mgenome.split("")
+    
+    if (document.querySelector("#Autosomal").checked == true) {
+        console.log("checked!")
+    } else if (document.querySelector("#Autosomal").checked == false) {
+        console.log("not checked!")
+    } 
+
+    
     return false
 }
